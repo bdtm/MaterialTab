cf2d0406b81ef81f5493426d669d4d987759e92c
bdtm <utomo.budi73@gmail.com>
2016-10-09 10:11:44 +0700

    versi 1.0

---
+++ app/src/main/java/com/dicoding/materialtab/adapter/TabFragmentPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.dicoding.materialtab.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import com.dicoding.materialtab.fragment.Tab1Fragment;
+import com.dicoding.materialtab.fragment.Tab2Fragment;
+
+/**
+ * Created by Asus on 8/24/2016.
+ */
+public class TabFragmentPagerAdapter  extends FragmentPagerAdapter {
+    //nama tab nya
+    String[] title = new String[]{
+            "Tab 1", "Tab 2"
+    };
+
+    public TabFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    //method ini yang akan memanipulasi penampilan Fragment dilayar
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new Tab1Fragment();
+                break;
+            case 1:
+                fragment = new Tab2Fragment();
+                break;
+            default:
+                fragment = null;
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+}
